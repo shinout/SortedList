e38a475c64e13378a8d6fa94faf34c7bcf9218a0
SHIN Suzuki <shinout310+git@gmail.com>
2013-06-14 02:39:03 +0900

    remove self-dependency

--- SortedList.js
+++ SortedList.js
@@ -1,5 +1,5 @@
 (function(root,factory) {
-  if (typeof define === 'function' && define.amd) define(['sortedlist'], factory);
+  if (typeof define === 'function' && define.amd) define([], factory);
   else if (typeof module == 'object' && module.exports) module.exports = factory();
   else root.SortedList = factory();
 }(this, function () {
