087cb98530c45071e2f06e520d7c811323e8d7ff
SHIN Suzuki <shinout310+git@gmail.com>
2013-06-14 04:08:54 +0900

    added some comments for volo

--- SortedList.js
+++ SortedList.js
@@ -1,3 +1,12 @@
+/*package.json
+{
+  "volo": {
+    "dependencies": {
+      "sortedlist": "github:shinout/SortedList"
+    }
+  }
+}
+*/
 (function(root,factory) {
   if (typeof define === 'function' && define.amd) define([], factory);
   else if (typeof module == 'object' && module.exports) module.exports = factory();
